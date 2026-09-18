mmoehabb/zigsdl

audit: simple rigidbody implementation has been finalized

src/scripts/rigidbody.zig
@@ -9,15 +9,14 @@ const Mesh = @import("./mesh.zig");
 const Rigidbody = @This();
 
 mass: f32, // TODO: use it in the update method logic
-gravity: bool = false,
+gravity: f32 = 0.00,
 static: bool = false,
 
-_G: f32 = 0.005, // The Gravitational Constant
-_vel: types.Vector = .{}, // Velocity
-_acc: types.Vector = .{}, // Acceleration
-_pfr: types.Vector = .{}, // Frictions of the position directions
-_nfr: types.Vector = .{}, // Frictions of the negative directions
+velocity: types.Vector = .{},
+acceleration: types.Vector = .{},
+_friction: types.Vector = .{},
 _collisions: std.ArrayList(types.Collision) = .empty,
+_impactors: std.ArrayList(*modules.Object) = .empty,
 
 _allocator: std.mem.Allocator,
 _script_strategy: modules.ScriptStrategy,
@@ -26,7 +25,7 @@ _collisionDetector: ?*plugins.CollisionDetector = null,
 pub fn init(data: struct {
     allocator: std.mem.Allocator,
     mass: f32 = 5,
-    gravity: bool = false,
+    gravity: f32 = 0.00,
     static: bool = false,
 }) !*Rigidbody {
     // Ensure required plugins are available
@@ -46,14 +45,15 @@ pub fn init(data: struct {
     };
     rigidbody._collisionDetector = cd;
     rigidbody._collisions = .empty;
-    rigidbody._vel = .{};
-    rigidbody._acc = .{};
-    rigidbody._G = 0.005;
+    rigidbody._impactors = .empty;
+    rigidbody.velocity = .{};
+    rigidbody.acceleration = .{};
     return rigidbody;
 }
 
 pub fn deinit(self: *Rigidbody) void {
     self._collisions.deinit(self._allocator);
+    self._impactors.deinit(self._allocator);
     self._allocator.destroy(self);
 }
 
@@ -68,10 +68,7 @@ fn start(s: *modules.Script, obj: *modules.Object) void {
     const self = @as(*Rigidbody, @constCast(
         @fieldParentPtr("_script_strategy", s.strategy),
     ));
-    if (self.static) {
-        self._pfr = .{ .x = 1.00, .y = 1.00, .z = 1.00 };
-        self._nfr = .{ .x = 1.00, .y = 1.00, .z = 1.00 };
-    } else {
+    if (!self.static) {
         if (modules.PluginManager.get(plugins.JammingResolver, "JammingResolver")) |jr| {
             jr.addObject(obj) catch std.log.warn(
                 "Rigidbody.start: couldn't add the object into the JammingResolver!",
@@ -81,54 +78,49 @@ fn start(s: *modules.Script, obj: *modules.Object) void {
     }
 }
 
-fn update(s: *modules.Script, obj: *modules.Object) void {
+fn update(s: *modules.Script, obj: *modules.Object, dt: f32) void {
     const self = @as(*Rigidbody, @constCast(
         @fieldParentPtr("_script_strategy", s.strategy),
     ));
     if (self.static) return;
 
-    // Motion Decay
-    self._vel.x -= if (self._vel.x > 0) self._vel.x * self._nfr.x else self._vel.x * self._pfr.x;
-    self._vel.y -= if (self._vel.y > 0) self._vel.y * self._nfr.y else self._vel.y * self._pfr.y;
-    self._vel.z -= if (self._vel.z > 0) self._vel.z * self._nfr.z else self._vel.z * self._pfr.z;
-
-    self._acc.x -= if (self._acc.x > 0) self._acc.x * self._nfr.x else self._acc.x * self._pfr.x;
-    self._acc.y -= if (self._acc.y > 0) self._acc.y * self._nfr.y else self._acc.y * self._pfr.y;
-    self._acc.z -= if (self._acc.z > 0) self._acc.z * self._nfr.z else self._acc.z * self._pfr.z;
-
-    // Motion Influence
-    obj.position = obj.position.add(self._vel);
-    _ = self.applyMomentum(self._acc);
-    if (self.gravity) _ = self.applyForce(.{ .y = @max(0, self._G) });
-
-    // Reset Frictions
-    self._pfr = self._pfr.multiply(0);
-    self._nfr = self._nfr.multiply(0);
-
-    // Detect collision, reslove jamming, and calculate frictions
+    // 1) Re-evaluate the friction vector based on the current collisions.
+    self._friction = self._friction.multiply(0);
     self._collisions.clearRetainingCapacity();
-    self._collisionDetector.?.getCollisions(obj, self._allocator, &self._collisions) catch unreachable;
+    self._collisionDetector.?.getCollisions(self._allocator, obj, &self._collisions) catch unreachable;
+
+    var vel_reactions = types.Vector{};
+    var acc_reactions = types.Vector{};
 
     for (self._collisions.items) |collision| {
-        const cobj = collision.face.owner;
+        var cobj = collision.face.owner;
+        if (cobj == obj) cobj = collision.myface.owner;
+
+        // skip this object if it's within the impactors list
+        var found = false;
+        for (self._impactors.items) |impactor| {
+            if (impactor == cobj) found = true;
+        }
+        if (found) continue;
+
         if (cobj.getScript(Rigidbody, "Rigidbody")) |rbody| {
-            inline for (collision.cps) |cp| {
-                if (cp) |cpoint| {
-                    const cx = @abs(cpoint.mag.x);
-                    const cy = @abs(cpoint.mag.y);
-                    // const cz = @abs(cpoint.z); TODO: enable z axis as well for 3D
-                    const mc = @min(cx, cy);
-                    if (mc == cx) {
-                        if (cpoint.mag.x > 0) self._pfr.x = @min(1.00, self._pfr.x + rbody._pfr.x) //
-                        else self._nfr.x = @min(1.00, self._nfr.x + rbody._nfr.x);
-                    } else if (mc == cy) {
-                        if (cpoint.mag.y > 0) self._pfr.y = @min(1.00, self._pfr.y + rbody._pfr.y) //
-                        else self._nfr.y = @min(1.00, self._nfr.y + rbody._nfr.y);
-                    }
-                }
-            }
+            std.debug.print("{s} -> {s}\n", .{ obj.name, cobj.name });
+            const reaction = rbody.impact(obj);
+            vel_reactions = vel_reactions.add(reaction.vel);
+            acc_reactions = acc_reactions.add(reaction.acc);
         }
     }
+    self._impactors.clearRetainingCapacity();
+
+    // 2) Apply motion decay using the evaluated friction.
+    _ = self.applyMomentum(vel_reactions);
+    _ = self.applyForce(acc_reactions);
+
+    // 3) Finally, move the object and update the rigidbody state.
+    self.velocity = self.velocity.add(self.acceleration);
+    obj.position = obj.position.add(self.velocity);
+
+    self.acceleration.y = @max(self.acceleration.y, 9.8 * self.gravity * dt);
 }
 
 fn end(_: *modules.Script, obj: *modules.Object) void {
@@ -140,16 +132,30 @@ fn end(_: *modules.Script, obj: *modules.Object) void {
 /// Apply force to the object and get a reaction force.
 /// NOTE: this mutates the inner state.
 pub fn applyForce(self: *Rigidbody, f: types.Vector) types.Vector {
-    if (self.static) return f.multiply(-1);
-    const res = self._acc.subtract(f);
-    self._acc = self._acc.add(f);
+    const res = self.acceleration.subtract(f);
+    if (!self.static) self.acceleration = self.acceleration.add(f);
     return res;
 }
 
 /// Apply momentum to the object and get a reaction momentum.
 /// NOTE: this mutates the inner state.
 pub fn applyMomentum(self: *Rigidbody, f: types.Vector) types.Vector {
-    if (self.static) return f.multiply(-1);
-    self._vel = self._vel.add(f);
-    return self._vel;
+    const res = self.velocity.subtract(f);
+    if (!self.static) self.velocity = self.velocity.add(f);
+    return res;
+}
+
+pub fn impact(self: *Rigidbody, obj: *modules.Object) struct {
+    vel: types.Vector = .{},
+    acc: types.Vector = .{},
+} {
+    if (obj.getScript(Rigidbody, "Rigidbody")) |rbody| {
+        if (!self.static) self._impactors.append(self._allocator, obj) catch unreachable;
+        if (self.velocity.isEql(rbody.velocity)) return .{};
+        return .{
+            .vel = self.applyMomentum(rbody.velocity),
+            .acc = self.applyForce(rbody.acceleration),
+        };
+    }
+    return .{};
 }

mmoehabb/zest

src/scripts/rigidbody.zig
@@ -1,4 +1,6 @@
 //! Add this script to any object in order to bestow it with physical properties.
+//! TODO: this component still needs a huge work to be complete; it should simulate
+//! the basic physics attributes... rigibodies should impact each other movements.
 
 const std = @import("std");
 const modules = @import("../modules/mod.zig");
@@ -9,14 +11,11 @@ const Mesh = @import("./mesh.zig");
 const Rigidbody = @This();
 
 mass: f32, // TODO: use it in the update method logic
-gravity: bool = false,
+gravity: f32 = 0.00,
 static: bool = false,
 
-_G: f32 = 0.005, // The Gravitational Constant
-_vel: types.Vector = .{}, // Velocity
-_acc: types.Vector = .{}, // Acceleration
-_pfr: types.Vector = .{}, // Frictions of the position directions
-_nfr: types.Vector = .{}, // Frictions of the negative directions
+velocity: types.Vector = .{},
+acceleration: types.Vector = .{},
 _collisions: std.ArrayList(types.Collision) = .empty,
 
 _allocator: std.mem.Allocator,
@@ -26,7 +25,7 @@ _collisionDetector: ?*plugins.CollisionDetector = null,
 pub fn init(data: struct {
     allocator: std.mem.Allocator,
     mass: f32 = 5,
-    gravity: bool = false,
+    gravity: f32 = 0.00,
     static: bool = false,
 }) !*Rigidbody {
     // Ensure required plugins are available
@@ -46,9 +45,8 @@ pub fn init(data: struct {
     };
     rigidbody._collisionDetector = cd;
     rigidbody._collisions = .empty;
-    rigidbody._vel = .{};
-    rigidbody._acc = .{};
-    rigidbody._G = 0.005;
+    rigidbody.velocity = .{};
+    rigidbody.acceleration = .{};
     return rigidbody;
 }
 
@@ -68,10 +66,7 @@ fn start(s: *modules.Script, obj: *modules.Object) void {
     const self = @as(*Rigidbody, @constCast(
         @fieldParentPtr("_script_strategy", s.strategy),
     ));
-    if (self.static) {
-        self._pfr = .{ .x = 1.00, .y = 1.00, .z = 1.00 };
-        self._nfr = .{ .x = 1.00, .y = 1.00, .z = 1.00 };
-    } else {
+    if (!self.static) {
         if (modules.PluginManager.get(plugins.JammingResolver, "JammingResolver")) |jr| {
             jr.addObject(obj) catch std.log.warn(
                 "Rigidbody.start: couldn't add the object into the JammingResolver!",
@@ -81,54 +76,24 @@ fn start(s: *modules.Script, obj: *modules.Object) void {
     }
 }
 
-fn update(s: *modules.Script, obj: *modules.Object) void {
+fn update(s: *modules.Script, obj: *modules.Object, dt: f32) void {
     const self = @as(*Rigidbody, @constCast(
         @fieldParentPtr("_script_strategy", s.strategy),
     ));
     if (self.static) return;
 
-    // Motion Decay
-    self._vel.x -= if (self._vel.x > 0) self._vel.x * self._nfr.x else self._vel.x * self._pfr.x;
-    self._vel.y -= if (self._vel.y > 0) self._vel.y * self._nfr.y else self._vel.y * self._pfr.y;
-    self._vel.z -= if (self._vel.z > 0) self._vel.z * self._nfr.z else self._vel.z * self._pfr.z;
-
-    self._acc.x -= if (self._acc.x > 0) self._acc.x * self._nfr.x else self._acc.x * self._pfr.x;
-    self._acc.y -= if (self._acc.y > 0) self._acc.y * self._nfr.y else self._acc.y * self._pfr.y;
-    self._acc.z -= if (self._acc.z > 0) self._acc.z * self._nfr.z else self._acc.z * self._pfr.z;
-
-    // Motion Influence
-    obj.position = obj.position.add(self._vel);
-    _ = self.applyMomentum(self._acc);
-    if (self.gravity) _ = self.applyForce(.{ .y = @max(0, self._G) });
-
-    // Reset Frictions
-    self._pfr = self._pfr.multiply(0);
-    self._nfr = self._nfr.multiply(0);
-
-    // Detect collision, reslove jamming, and calculate frictions
     self._collisions.clearRetainingCapacity();
-    self._collisionDetector.?.getCollisions(obj, self._allocator, &self._collisions) catch unreachable;
-
-    for (self._collisions.items) |collision| {
-        const cobj = collision.face.owner;
-        if (cobj.getScript(Rigidbody, "Rigidbody")) |rbody| {
-            inline for (collision.cps) |cp| {
-                if (cp) |cpoint| {
-                    const cx = @abs(cpoint.mag.x);
-                    const cy = @abs(cpoint.mag.y);
-                    // const cz = @abs(cpoint.z); TODO: enable z axis as well for 3D
-                    const mc = @min(cx, cy);
-                    if (mc == cx) {
-                        if (cpoint.mag.x > 0) self._pfr.x = @min(1.00, self._pfr.x + rbody._pfr.x) //
-                        else self._nfr.x = @min(1.00, self._nfr.x + rbody._nfr.x);
-                    } else if (mc == cy) {
-                        if (cpoint.mag.y > 0) self._pfr.y = @min(1.00, self._pfr.y + rbody._pfr.y) //
-                        else self._nfr.y = @min(1.00, self._nfr.y + rbody._nfr.y);
-                    }
-                }
-            }
-        }
+    self._collisionDetector.?.getCollisions(self._allocator, obj, &self._collisions) catch unreachable;
+    for (self._collisions.items) |_| {
+        self.acceleration = self.acceleration.multiply(0);
+        self.velocity = self.velocity.multiply(0);
+        return;
     }
+
+    self.velocity = self.velocity.add(self.acceleration);
+    obj.position = obj.position.add(self.velocity);
+
+    self.acceleration.y = @max(self.acceleration.y, 9.8 * self.gravity * dt);
 }
 
 fn end(_: *modules.Script, obj: *modules.Object) void {
@@ -140,16 +105,15 @@ fn end(_: *modules.Script, obj: *modules.Object) void {
 /// Apply force to the object and get a reaction force.
 /// NOTE: this mutates the inner state.
 pub fn applyForce(self: *Rigidbody, f: types.Vector) types.Vector {
-    if (self.static) return f.multiply(-1);
-    const res = self._acc.subtract(f);
-    self._acc = self._acc.add(f);
+    const res = self.acceleration.subtract(f);
+    if (!self.static) self.acceleration = self.acceleration.add(f);
     return res;
 }
 
 /// Apply momentum to the object and get a reaction momentum.
 /// NOTE: this mutates the inner state.
 pub fn applyMomentum(self: *Rigidbody, f: types.Vector) types.Vector {
-    if (self.static) return f.multiply(-1);
-    self._vel = self._vel.add(f);
-    return self._vel;
+    const res = self.velocity.subtract(f);
+    if (!self.static) self.velocity = self.velocity.add(f);
+    return res;
 }